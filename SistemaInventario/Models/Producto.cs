5981693ed4a11ee871e5f518cc17f120a430c373
Daniel <barreraquezadadanielalejandro@gmail.com>
2025-09-11 22:30:58 -0600

    Creacion del sistema de inventario

---
+++ SistemaInventario/Models/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaInventario.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        [StringLength(250)]
+        [Required]
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        [Required]
+        public double Precio { get; set; }
+        public int Stock { get; set; }
+        public int ProveedorId { get; set; }
+        public Proveedor Proveedor { get; set; }
+    }
+}
---
+++ SistemaInventario/Models/Proveedor.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaInventario.Models
+{
+    public class Proveedor
+    {
+        public int Id { get; set; }
+        [StringLength(250)]
+        [Required]
+        public string Nombre { get; set; }
+        public string Contacto { get; set; }
+        public  ICollection<Producto> Productos { get; set; }
+    }
+}
